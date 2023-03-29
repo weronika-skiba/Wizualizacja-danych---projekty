ef99e9e76477f0b988bf2baf86daafdea2bd29a1
mihawb <michalbanaszczak@gmail.com>
2023-03-29 15:31:35 +0200

    wykresy zaleznosci miedzy cechami

--- MATLAB/whitewine.m
+++ MATLAB/whitewine.m
@@ -10,7 +10,10 @@ white = readtable('../datasets/winequality-white.csv');
 summary(white);
 
 %% rozkład ocen jakości
-figure(1);
+fig1 = figure(1);
+fig1.Position(3:4) = [900, 400];
+
+subplot(1,2,1);
 hist = histogram(white.quality);
 title('Rozkład ocen jakości');
 
@@ -19,39 +22,27 @@ binvals = hist.Values;
 
 text(binedges(1:end-1), binvals, num2str(binvals'), 'vert', 'bottom', ...
     'horiz', 'center');
+
+subplot(1,2,2);
+qint8 = uint8(white.quality);
+[Q, QG, QP] = groupcounts(qint8);
+pie = pie(Q, '%.2f%%');
+legend({'3','4','5','6','7','8','9'});
     
 %% rozkład ilości alkoholu
-figure(1);
+figure(2);
 hist = histogram(white.alcohol);
 title('Rozkład alkoholu ');
 
-binedges = hist.BinEdges + 0.5;
-binvals = hist.Values;
-
-text(binedges(1:end-1), binvals, num2str(binvals'), 'vert', 'bottom', ...
-    'horiz', 'center');
-
 %% rozkład ilości siarki
-figure(1);
+figure(3);
 hist = histogram(white.freeSulfurDioxide, 'BinLimits', [0, 100]);
 title('Rozkład ilości siarki ');
 
-binedges = hist.BinEdges + 0.5;
-binvals = hist.Values;
-
-text(binedges(1:end-1), binvals, num2str(binvals'), 'vert', 'bottom', ...
-    'horiz', 'center');
-
 %% rozkład ilości cukru
-figure(1);
+figure(4);
 hist = histogram(white.residualSugar, 'BinLimits', [0, 21]);
 title('Rozkład ilości cukru ');
-
-binedges = hist.BinEdges + 0.5;
-binvals = hist.Values;
-
-text(binedges(1:end-1), binvals, num2str(binvals'), 'vert', 'bottom', ...
-    'horiz', 'center');
 
 %% korelacja między cechami i heatmapy
 
@@ -61,7 +52,48 @@ white_cols = white.Properties.VariableNames;
 % diagonala=1 zaburza skalę kolorów gdy najwyższa korelacja~=0.8
 white_corr = corrcoef(white_ndarray) - eye(white_size(2));
 
-figure(2);
+figure(5);
 heat = heatmap(white_cols, white_cols, white_corr);
 title('Współczynniki korelacji między cechami');
 heat.Colormap = parula;
+
+%% cos tam
+
+fig6 = figure(6);
+fig6.Position = [100, 100, 1500, 800];
+
+subplot(2,3,1);
+scatter(white.density, white.alcohol,10,white.quality,'filled');
+title('% w funkcji gęstości');
+colorbar
+colormap cool
+
+subplot(2,3,2);
+scatter(white.residualSugar, white.density,10,white.quality,'filled');
+title('słodkość w funkcji gęstości');
+colorbar
+colormap cool
+
+subplot(2,3,3);
+scatter(white.totalSulfurDioxide, white.density,10,white.quality,'filled');
+title('SO2 w funkcji gęstości');
+colorbar
+colormap cool
+
+subplot(2,3,4);
+scatter(white.pH, white.fixedAcidity,10,white.quality,'filled');
+title('pH od zawartości kwasów lotnych');
+colorbar
+colormap cool
+
+subplot(2,3,5);
+scatter(white.alcohol, white.residualSugar,10,white.quality,'filled');
+title('% w funkcji słodkości');
+colorbar
+colormap cool
+
+subplot(2,3,6);
+scatter(white.alcohol, white.totalSulfurDioxide,10,white.quality,'filled');
+title('% od zawartości SO2');
+colorbar
+colormap cool
