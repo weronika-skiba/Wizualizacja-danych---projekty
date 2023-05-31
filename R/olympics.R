4f459ae27903e951548eda6183e022de2b16c5f4
mihawb <michalbanaszczak@gmail.com>
2023-05-31 16:27:09 +0200

    polympics

--- R/olympics.R
+++ R/olympics.R
@@ -10,7 +10,7 @@ world <- ne_countries(scale = "medium", returnclass = "sf")
 olympics <- read.csv('../datasets/olympics.csv', sep = ";")
 
 # zliczenie medali na panstwo, dodanie tego do world dla wygody
-allMedals = olympics[olympics$Place >= 3,]
+allMedals = olympics[olympics$Place <= 3,]
 goldMedals = olympics[olympics$Place == 1,]
 
 allMedalsCount <- allMedals %>%
@@ -47,7 +47,7 @@ allmeds_count_slice = allMedalsCount[order(allMedalsCount$total_count, decreasin
 ggplot(allmeds_count_slice, aes(x=reorder(Country, desc(total_count)), y=total_count)) + 
   geom_bar(stat = "identity") + ggtitle("Wszystkie zdobyte medale") 
 
-# wykres 2 - mapa, medale na siwecie
+# wykres 2 - mapa, medale na swiecie
 ggplot(data = world) +
   geom_sf(aes(fill = allMeds)) +
   scale_fill_viridis_c(option = "plasma", trans = "sqrt") +
@@ -59,9 +59,39 @@ goldmeds_count_slice = goldMedalsCount[order(goldMedalsCount$total_count, decrea
 ggplot(goldmeds_count_slice, aes(x=reorder(Country, desc(total_count)), y=total_count)) + 
   geom_bar(stat = "identity") + ggtitle("Zdobyte złote medale") 
 
-# wykres 4 - mapa, zlote medale na siwecie
+# wykres 4 - mapa, zlote medale na swiecie
 ggplot(data = world) +
   geom_sf(aes(fill = goldMeds)) +
   scale_fill_viridis_c(option = "plasma", trans = "sqrt") +
   xlab("Longitude") + ylab("Latitude") +
   ggtitle("Zdobyte złote medale") 
+
+# agregacja polskich wyników co roku
+polympics = olympics[olympics$Country == 'Poland',]
+polympics = na.omit(polympics)
+
+avg_place = aggregate(polympics$Place, list(polympics$Year), mean)
+sum_allmeds = polympics[polympics$Place <= 3,] %>%
+              count(Year, name="all_count")
+sum_goldmeds = polympics %>%
+              group_by(Year) %>%
+              summarize(gold_count = sum(Place == 1))
+
+# wykres 5 - wyniki polskich sportowcow w czasie
+plot(avg_place$Group.1, avg_place$x, type = "b", pch = 20, 
+     col = "red", xlab = "Rok", ylab = "Ilość medali", ylim = c(0, 70))
+lines(sum_goldmeds$Year, sum_goldmeds$gold_count, pch = 20, col = "blue", type = "b")
+lines(sum_allmeds$Year, sum_allmeds$all_count, pch = 20, col = "green", type = "b")
+legend("topleft", legend=c("Średnie miejsce", "Złote medale", "Wszystkie medale"),
+       col=c("red", "blue", "green"), lty = 1:1:1, cex=0.8)
+title('Wyniki polskich sportowców')
+
+# wykres 6 - wazny polski wykres
+plot(avg_place$Group.1, avg_place$x, type = "b", pch = 20, 
+     col = "red", xlab = "Rok", ylab = "Ilość medali", ylim = c(0, 70))
+lines(sum_goldmeds$Year, sum_goldmeds$gold_count, pch = 20, col = "blue", type = "b")
+lines(sum_allmeds$Year, sum_allmeds$all_count, pch = 20, col = "green", type = "b")
+legend("topleft", legend=c("Średnie miejsce", "Złote medale", "Wszystkie medale","Pontyfikat Jana Pawła II"),
+       col=c("red", "blue", "green","yellow"), lty = 1:1:1:2, cex=0.8)
+abline(v=c(1978,2005), col=c("yellow", "yellow"), lty=c(2,2), lwd=c(3, 3))
+title('Wpływ objęcia pontyfikatu przez Jana Pawła II na wyniki polskich sportowców')
